363d713afcdee8e1877e9a768dc14125c7ed150e
Administrator666666 <alexzou2021@163.com>
2025-02-01 19:02:36 +0800

    Add files via upload

---
+++ MS-DOS.py
@@ -0,0 +1,22 @@
+print('MS-DOS is starting...')
+print('MICROSOFT COPRORATION')
+sum==input('Enter your name: ')
+if sum=='Alex':
+    print('Welcome back Alex!')
+else:
+    print('Access denied!')
+print('C:\>')
+d=input()
+if d=='dir':
+    print('Volume in drive C has no label')
+    print('Volume Serial Number is 0000-0000')
+    print('Programs')
+    print('Documents')
+    print('Games')
+    print('Users')
+    print('C:\>')
+a = input()
+if a=='exit':
+    print('Exiting MS-DOS...')
+else:
+    print('Invalid command')
